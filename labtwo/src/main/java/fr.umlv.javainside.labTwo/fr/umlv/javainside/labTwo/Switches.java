c362984f58d8f61e11454c79f7dc123016438670
BAULAY LAURENT <baulaylaurent@gmail.com>
2018-10-08 12:11:11 +0200

    Ajout de la methode intSwitch2 dans la classe Switches.java et modification des tests unitaires.

--- labtwo/src/main/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/Switches.java
+++ labtwo/src/main/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/Switches.java
@@ -3,10 +3,18 @@ package fr.umlv.javainside.labTwo;
 public class Switches {
 	public static String intSwitch(int nombre) {
 		switch(nombre) {
-		case 0 : return "zero";
-		case 1 : return "one";
-		case 2 : return "a lot";
-		default : throw new IllegalArgumentException("number should be 0, 1 or 2. Having : " + nombre);
+			case 0 : return "zero";
+			case 1 : return "one";
+			case 2 : return "a lot";
+			default : throw new IllegalArgumentException("number should be 0, 1 or 2. Having : " + nombre);
+		}
+	}
+	public static String intSwitch2(int nombre) {
+		switch(nombre) {
+			case 0 : return "zero";
+			case 10 : return "one";
+			case 100 : return "a lot";
+			default : throw new IllegalArgumentException("number should be 0, 1 or 2. Having : " + nombre);
 		}
 	}
 }
--- labtwo/src/test/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/SwitchesTests.java
+++ labtwo/src/test/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/SwitchesTests.java
@@ -2,17 +2,53 @@ package fr.umlv.javainside.labTwo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SwitchesTests {
-
-	
-	@Test
-	void intSwitchtest() {
-		assertEquals("one", Switches.intSwitch(1));
-		assertEquals("a lot", Switches.intSwitch(2));
-		assertEquals("zero", Switches.intSwitch(0));
-		assertThrows(IllegalArgumentException.class, () -> Switches.intSwitch(-1));
+	static class TestData {
+		IntFunction function;
+		int parameter;
+		String expectedReturn;
+		
+		public TestData(IntFunction function,int parameter, String expectedReturn) {
+			this.function = function;
+			this.parameter = parameter;
+			this.expectedReturn = expectedReturn;
+		}
+		public IntFunction getFunction() {
+			return function;
+		}
+		public int getParameter() {
+			return parameter;
+		}
+		public String getExpectedReturn() {
+			return expectedReturn;
+		}
 	}
-
+	
+	@ParameterizedTest
+	@MethodSource("testDataProvider")
+	void intSwitchtest(TestData function) {
+		assertEquals(function.expectedReturn, function.getFunction().apply(function.getParameter()));
+	}
+	@ParameterizedTest
+	@MethodSource("getIntFunction")
+	void intSwitchtestError(IntFunction<String> function) {
+		assertThrows(IllegalArgumentException.class, () -> function.apply(-1));
+	}
+	static Stream<TestData> testDataProvider() {
+	    List<TestData> lst = List.of(new TestData(Switches::intSwitch, 0, "zero"), new TestData(Switches::intSwitch, 1, "one"), new TestData(Switches::intSwitch, 2, "a lot"), new TestData(Switches::intSwitch2, 0, "zero"), new TestData(Switches::intSwitch2, 10, "one"), new TestData(Switches::intSwitch2, 100, "a lot"));
+	    return lst.stream();
+	}
+	static Stream<IntFunction> getIntFunction() {
+		List<IntFunction> lst = List.of(Switches::intSwitch, Switches::intSwitch2);
+		return lst.stream();
+	}
 }
