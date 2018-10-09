3adcefd890387a80274dfe14e93fc7080cf37d3a
BAULAY LAURENT <baulaylaurent@gmail.com>
2018-10-09 09:48:51 +0200

    Debut du lab03 et modification du .travis.yml et correction du module-info du labtwo

--- labtwo/src/test/java/fr.umlv.javainside.labTwo/module-info.java
+++ labtwo/src/test/java/fr.umlv.javainside.labTwo/module-info.java
@@ -1,6 +1,6 @@
 open module fr.umlv.javainside.labTwo {
   requires org.junit.jupiter.api;
-
+  requires org.junit.jupiter.params;
   // requires org.openjdk.jmh;  // JMH support
   // requires org.openjdk.jmh.generator;
 }
