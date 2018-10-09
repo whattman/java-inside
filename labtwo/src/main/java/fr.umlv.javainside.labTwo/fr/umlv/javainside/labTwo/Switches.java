a83ca2160af1e4b6355832b175aef8c3f35d51f0
Baulay Laurent <lbaulay@etud.u-pem.fr>
2018-10-09 11:14:21 +0200

    Finalisation du lab02

--- labtwo/src/main/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/Switches.java
+++ labtwo/src/main/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/Switches.java
@@ -1,6 +1,10 @@
 package fr.umlv.javainside.labTwo;
 
 public class Switches {
+	public static enum EnumSwitch {
+		DEBUG, WARNING, INFO, ERROR
+	}
+	
 	public static String intSwitch(int nombre) {
 		switch(nombre) {
 			case 0 : return "zero";
@@ -17,4 +21,23 @@ public class Switches {
 			default : throw new IllegalArgumentException("number should be 0, 1 or 2. Having : " + nombre);
 		}
 	}
+	public static String StringSwitch(String index) {
+		switch(index) {
+			case "viva zorg" :
+			case "foo": return "zero";
+			case "bar" : return "one";
+			case "baz" : return "a lot";
+			default : throw new IllegalArgumentException("number should be foo, bar, baz or viva zorg. Having : " + index);
+		}
+	}
+	public static String enumSwitch(EnumSwitch e) {
+		switch(e) {
+			case ERROR :
+			case DEBUG: return "zero";
+			case WARNING : return "one";
+			case INFO : return "a lot";
+			default: throw new IllegalArgumentException("number should be foo, bar, baz or viva zorg. Having : " + e);
+		}
+	}
+	
 }
--- labtwo/src/test/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/SwitchesTests.java
+++ labtwo/src/test/java/fr.umlv.javainside.labTwo/fr/umlv/javainside/labTwo/SwitchesTests.java
@@ -1,13 +1,18 @@
 package fr.umlv.javainside.labTwo;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import java.util.function.IntFunction;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import fr.umlv.javainside.labTwo.Switches.EnumSwitch;
 
 
 
@@ -43,8 +48,27 @@ class SwitchesTests {
 	void intSwitchtestError(IntFunction<String> function) {
 		assertThrows(IllegalArgumentException.class, () -> function.apply(-1));
 	}
+	
+	@Test
+	void StringSwitchtest() {
+		assertEquals("zero", Switches.StringSwitch("foo"));
+		assertEquals("one", Switches.StringSwitch("bar"));
+		assertEquals("a lot", Switches.StringSwitch("baz"));
+		assertEquals("zero", Switches.StringSwitch("viva zorg"));
+	}
+	@Test
+	void StringSwitchtestError() {
+		assertThrows(IllegalArgumentException.class, () -> Switches.StringSwitch("Toto"));
+	}
+	@Test
+	void EnumSwitchtest() {
+		assertEquals("zero", Switches.enumSwitch(EnumSwitch.DEBUG));
+		assertEquals("one", Switches.enumSwitch(EnumSwitch.WARNING));
+		assertEquals("a lot", Switches.enumSwitch(EnumSwitch.INFO));
+		assertEquals("zero", Switches.enumSwitch(EnumSwitch.ERROR));
+	}
 	static Stream<TestData> testDataProvider() {
-	    List<TestData> lst = List.of(new TestData(Switches::intSwitch, 0, "zero"), new TestData(Switches::intSwitch, 1, "one"), new TestData(Switches::intSwitch, 2, "a lot"), new TestData(Switches::intSwitch2, 0, "zero"), new TestData(Switches::intSwitch2, 10, "one"), new TestData(Switches::intSwitch2, 100, "a lot"));
+	    List<TestData> lst = List.of(new TestData(Switches::intSwitch, 0, "zero"), new TestData(Switches::intSwitch, 3, "zero"), new TestData(Switches::intSwitch2, 3, "zero"),new TestData(Switches::intSwitch, 1, "one"), new TestData(Switches::intSwitch, 2, "a lot"), new TestData(Switches::intSwitch2, 0, "zero"), new TestData(Switches::intSwitch2, 10, "one"), new TestData(Switches::intSwitch2, 100, "a lot"));
 	    return lst.stream();
 	}
 	static Stream<IntFunction> getIntFunction() {
