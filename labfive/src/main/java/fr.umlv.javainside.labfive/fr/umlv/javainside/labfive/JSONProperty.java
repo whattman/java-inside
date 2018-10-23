957dc1e972021d0a0f5f057c83f6685324b004c7
BAULAY LAURENT <baulaylaurent@gmail.com>
2018-10-23 11:28:50 +0200

    Ajout de l'annotation documented

--- labfive/src/main/java/fr.umlv.javainside.labfive/fr/umlv/javainside/labfive/JSONProperty.java
+++ labfive/src/main/java/fr.umlv.javainside.labfive/fr/umlv/javainside/labfive/JSONProperty.java
@@ -2,9 +2,11 @@ package fr.umlv.javainside.labfive;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.RetentionPolicy;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface JSONProperty {
