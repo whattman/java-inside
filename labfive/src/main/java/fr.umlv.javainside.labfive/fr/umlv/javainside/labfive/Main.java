0a3b1c5e51f9ae3caddc490368dff8653ca9b167
BAULAY LAURENT <baulaylaurent@gmail.com>
2018-10-24 14:31:36 +0200

    Modification du cache pour cacher uniquement les methode ayant l'annotation JSONProperties et le nom qu'on va utiliser

--- labfive/src/main/java/fr.umlv.javainside.labfive/fr/umlv/javainside/labfive/Main.java
+++ labfive/src/main/java/fr.umlv.javainside.labfive/fr/umlv/javainside/labfive/Main.java
@@ -6,12 +6,26 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
-	private static ClassValue<Method[]> cache = new ClassValue<Method[]>() {
+	private static class CachedInfos {
+		private final Method method;
+		private final String propertiesName;
+		
+		public CachedInfos(Method method, String propertiesName) {
+			this.method = method;
+			this.propertiesName = propertiesName;
+		}
+	}
+	private static ClassValue<List<CachedInfos>> cache = new ClassValue<List<CachedInfos>>() {
 		@Override
-		protected Method[] computeValue(Class<?> arg0) {
-			return arg0.getMethods();
+		protected List<CachedInfos> computeValue(Class<?> arg0) {
+			return Arrays.stream(arg0.getMethods())
+					.filter(method -> method.isAnnotationPresent(JSONProperty.class))
+					.map(method -> new CachedInfos(method, getmethodNameOrAlternativeName(method)))
+					.collect(Collectors.toList());
 		}
 	};
 	private static String propertyName(String name) {
@@ -33,20 +47,18 @@ public class Main {
 			throw new AssertionError(e);
 		}
 	}
-	private static String getmethodNameOrAlternativeName(Method m) {
-		var alterativeName = m.getAnnotation(JSONProperty.class).alterativeName();
-		return alterativeName.equals("") ? propertyName(m.getName()) : alterativeName;
+	private static String getmethodNameOrAlternativeName(Method method) {
+		var alterativeName = method.getAnnotation(JSONProperty.class).alterativeName();
+		return alterativeName.equals("") ? propertyName(method.getName()) : alterativeName;
 	}
 	public static String toJSON(Object obj) {
-		return Arrays.stream(cache.get(obj.getClass()))
-		    .filter(method -> method.isAnnotationPresent(JSONProperty.class))
-			.map(method -> formatJSONLine(obj, method))
+		return cache.get(obj.getClass()).stream()
+			.map(cachedInfo -> formatJSONLine(obj, cachedInfo))
 			.collect(joining(",\n", "{\n", "\n}"));
 	}
-	private static String formatJSONLine(Object obj, Method method) {
-		return "\t\"" + getmethodNameOrAlternativeName(method) + "\": \"" + doInvoke(method, obj) + "\"";
+	private static String formatJSONLine(Object obj, CachedInfos ci) {
+		return "\t\"" + ci.propertiesName + "\": \"" + doInvoke(ci.method, obj) + "\"";
 	}
-	
 	public static void main(String[] args) {
 	    var person = new Person("John", "Doe");
 	    System.out.println(toJSON((Object)person));
